c5eda9d9f69d6cfa7bcf55105956dbe9af61bab3
Domagoj Licitar <licitar.domagoj@gmail.com>
2021-10-19 14:56:53 +0200

    Exercise 2 Adding CustomerList and MoviesList

---
+++ Vidley/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+
+namespace Vidley.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details (int id)
+        {
+            var movies = GetMovies().SingleOrDefault(m => m.Id == id);
+            return View(movies);
+        }
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{ Id = 1, Name = "Sherlock Holmes" },
+                new Movie{ Id = 2, Name = "Star Wars" }
+            };
+        }
+    }
+}
