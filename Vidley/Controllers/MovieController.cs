3fa05aeeed1cca6672f5ce3c913abb09c5fdb326
Domagoj Licitar <licitar.domagoj@gmail.com>
2021-10-20 14:47:22 +0200

    Exercise 4 Creating forms for Movie list

--- Vidley/Controllers/MovieController.cs
+++ Vidley/Controllers/MovieController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidley.Models;
 using System.Data.Entity;
+using Vidley.ViewModels;
 
 namespace Vidley.Controllers
 {
@@ -18,6 +19,51 @@ namespace Vidley.Controllers
         protected override void Dispose(bool disposing)
         {
             _dbContext.Dispose();
+        }
+        public ActionResult New()
+        {
+            var genres = _dbContext.Genres.ToList();
+            var viewModel = new MovieViewModel
+            {
+                Genres = genres
+            };
+
+            return View("FormMovies", viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movies = _dbContext.Movies.SingleOrDefault(m => m.Id == id);
+            if (movies == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieViewModel
+            {
+                Movie = movies,
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            return View("FormMovies", viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _dbContext.Movies.Add(movie);
+            }
+            else
+            {
+                var movieDb = _dbContext.Movies.Single(m => m.Id == movie.Id);
+                
+                movieDb.Name = movie.Name;
+                movieDb.GenreType = movie.GenreType;
+                movieDb.DateAdded = movie.DateAdded;
+                movieDb.ReleseDate = movie.ReleseDate;
+                movieDb.NumberInStock = movie.NumberInStock;
+            }
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index","Movie");
         }
         public ViewResult Index()
         {
--- Vidley/Models/Movie.cs
+++ Vidley/Models/Movie.cs
@@ -9,13 +9,24 @@ namespace Vidley.Models
     public class Movie
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name ="Relese date")]
         public DateTime ReleseDate { get; set; }
+        
+        [Display(Name ="Date Added")]
         public DateTime DateAdded { get; set; }
+        
+        [Display(Name ="Number in Stock")]
         public int NumberInStock { get; set; }
+        
         public Genre Genre { get; set; }
+        
+        [Required]
+        [Display(Name = "Genre")]
         public int GenreType { get; set; }
     }
 }
